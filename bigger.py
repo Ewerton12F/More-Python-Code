29d40e5472cb155b2f1e1ed35210e7c97d2ea3a0
Ewerton12F <ewerton12freitas@gmail.com>
2022-03-31 17:29:04 -0300

    Recursion

---
+++ bigger.py
@@ -0,0 +1,15 @@
+def bigger_rec(l):
+    lengh = len(l)
+    if lengh == 1:
+        return 0
+    last = l[-1]
+    sub_l = bigger_rec(l[:-1])
+    if last > sub_l:
+        bigger = last
+    else:
+        bigger = sub_l
+    return bigger
+
+l = [1,0,1,2,3,4,10,5,6,7,8,9,11,10]
+
+print(bigger_rec(l))
